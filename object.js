e82ad0afb55d51de5062c4ac9ef48a05283dd9be
Ivan Limongan <ivan.limongan@gmail.com>
2016-03-24 21:30:04 -0700

    final commit of this lab

--- object.js
+++ object.js
@@ -1,13 +1,14 @@
 console.log("test")
 
 // Cookie Store object
-var cookieStore = function(storeLocation, minCust, maxCust, avgSale, openHours) {
+var cookieStore = function(storeLocation, minCust, maxCust, avgSale, openHours, locationID) {
   this.storeLocation = storeLocation;
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.avgSale = avgSale;
   this.openHours = openHours;
   this.salesArray = [];
+  this.locationID = locationID;
 }
 
 // Generates random number of customer per hour
@@ -32,12 +33,18 @@ cookieStore.prototype.dailySaleTotal = function() {
   return total;
 }
 
+cookieStore.prototype.addTitle = function() {
+  document.write("<ul id='locationID'>test</ul>").appendChild;
 
-var pioneerSquare = new cookieStore("Pioneer Square", 17, 18, 5.2, 8);
-var portlandAirport = new cookieStore("Portland Airport", 6, 24, 1.2, 8);
-var washingtonSquare = new cookieStore("Washington Square", 11, 38, 1.9, 8);
-var sellwood = new cookieStore("Sellwood", 20, 48, 3.3, 8);
-var pearlDistrict = new cookieStore("Pearl District", 3, 24, 2.6, 8);
+}
+
+
+
+var pioneerSquare = new cookieStore("Pioneer Square", 17, 18, 5.2, 8, "pioneer");
+var portlandAirport = new cookieStore("Portland Airport", 6, 24, 1.2, 8, "portland");
+var washingtonSquare = new cookieStore("Washington Square", 11, 38, 1.9, 8, "washington");
+var sellwood = new cookieStore("Sellwood", 20, 48, 3.3, 8, "sellwood");
+var pearlDistrict = new cookieStore("Pearl District", 3, 24, 2.6, 8, "pearl");
 
 console.log(pioneerSquare.dailySaleTotal());
 console.log(pioneerSquare.salesArray);
